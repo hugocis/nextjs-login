970a6b7fa2fd88c8238f3554bc4a51985a0f86f2
hugocis <9101917@alumnos.ufv.es>
2024-10-23 13:16:32 +0200

    Fix for deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { LoadNotes } from "@/app/lib/service"
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal, AwaitedReactNode } from "react";
 async function HomePage(){
 
   const data = await LoadNotes();
   return(
     <div>{
-      data.map( (note) =>{
+      data.map( (note: { id: Key | null | undefined; title: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined; content: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined; }) =>{
         <div key ={note.id}>
         <h1>{note.title} </h1>
         <p>{note.content}</p>
