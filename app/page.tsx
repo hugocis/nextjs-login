cf3c3639056fdd38d40b15b24744786e8ed35e89
hugocis <9101917@alumnos.ufv.es>
2024-10-24 10:41:36 +0200

    Deploying

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,31 @@
 // import { loadNotes }   from "@/app/lib/service"
 import { NoteForm } from "@/app/components/NoteForm"
 
-async function loadNotes(){
-  const res = await fetch('http://localhost:3000/api/notes');
-  const data = await res.json();
-  return data;
-}
+// async function loadNotes(){
+//   const res = await fetch('http://localhost:3000/api/notes');
+//   const data = await res.json();
+//   return data;
+// }
 
 
 
 async function HomePage() {
   
-  const data = await loadNotes();
+  //const data = await loadNotes();
 
   return(
   <div className="flex items-center justify-center h-screen">
     <div>
       <NoteForm />
        
-      <div>{
+      {/* <div>{
         data.map((note : any) => {
           <div key={note.id} className="bg-slate-900 p-4 my-2">
             <h1>{note.title} </h1>
             <p>{note.content}</p>
           </div>
         })
-      }</div>
+      }</div> */}
     </div>
   </div>
   )
